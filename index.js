8260d41fef6b1ffea781afe2a35fe1c5d8dd51df
Leandra Cartaxo <116728228+cartaxoleandra@users.noreply.github.com>
2022-12-21 16:40:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,80 +1,156 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const login = document.getElementById('login')
-const email = document.getElementById('email')
-const password= document.getElementById('password')
-const passconfirmation = document.getElementById('passconfirmation')
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
+function adicionar() {
+    const conteudo = document.getElementById('texto').value;
+    const novaH2 = document.createElement('H2');
+    const novoBotao = document.createElement('button');
+    novoBotao.innerHTML = "Excluir";
+    novaH2.innerHTML = conteudo;
 
-    checkInputs();
-});
-function checkInputs() {
-    const usernameValue = username.value;
-    const loginValue = login.value;
-    const emailValue = email.value;
-    const passwordValue = password.value;
-    const passconfirmationValue = passconfirmation.value;
+    novaH2.addEventListener('click', function () {
+        novaH2.style.color = "rgb(20,9,0)";
+        novaH2.style.textDecoration = "line-through"
+    })
 
-    if (usernameValue === '') {
-        setErrorFor(username, "Todos os campos são obrigatórios");
-    } else if (usernameValue.length <10){
-        setErrorFor(username, "Precisa ter no mínimo 10 caracteres");
-    } else {
-        setSuccessFor(username);
-    }
-    if (loginValue ==='') {
-        setErrorFor (login, "Todos os campos são obrigatórios");
-    } else if (loginValue.length>10){
-        setErrorFor(login, "Precisa ter no máximo 10 caracteres");
-    } else{
-        setSuccessFor(login);
-    }
-    if (emailValue === ''){
-        setErrorFor(email, "Todos os campos são obrigatórios");
-    } else if (!checkEmail(emailValue)){
-        setErrorFor(email, "Insira um Email válido!");
-    } else {
-        setSuccessFor(email);
-    }
-    if  (passwordValue === ""){
-        setErrorFor(password, "Todos os campos são obrigatórios");
-    } else {
-        setSuccessFor(password);
-    }
-    if (passconfirmationValue ===''){
-        setErrorFor(passconfirmation, "Todos os campos são obrigatórios");
-    } else if (passconfirmationValue != passwordValue){
-        setErrorFor(passconfirmation, "As senhas não conferem");
-    } else {
-        setSuccessFor(passconfirmation);
-    }
-}
-function setErrorFor (input, message){
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
+    novoBotao.addEventListener('click', function () {
+        novaH2.remove();
+        novoBotao.remove();
+    })
 
-    //Adicionar a mensagem de erro
-    small.innerText = message;
-
-    //Adicionar a classe de erro
-    formControl.className = 'form-control error';
-
-}
-function setSuccessFor(input, message){
-    const formControl = input.parentElement;
- 
-    //Adicionar a classe de sucesso
-    formControl.className = 'form-control success';
-
+    document.body.appendChild(novaH2);
+    document.body.appendChild(novoBotao);
+    document.getElementById('texto').value = "";
 }
 
-function checkEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email
-    );
-  }
+function cadastrar() {   
+    document.getElementById('cadastrar').style.display = "none";
+    document.getElementById('exibir').style.display = "block";
+    document.getElementById('mostraDados').style.display = "none";
+
+    document.getElementsByTagName('h2')[0].style.display = "block";
+}
+
+function exibir() {
+    document.getElementById('cadastrar').style.display = "block";
+    document.getElementById('exibir').style.display = "none";
+    document.getElementById('mostraDados').style.display = "none";
+
+    document.getElementsByTagName('h2')[0].style.display = "block";
+}
+
+function dados() {
+
+    document.getElementById('cadastrar').style.display = "none";
+    document.getElementById('exibir').style.display = "none";
+    document.getElementById('mostraDados').style.display = "block";
+
+    document.getElementById('mostraNome').innerText = nome.value;
+    document.getElementById('mostraLogin').innerText = login.value;
+    document.getElementById('mostraMail').innerText = email.value;
+    document.getElementById('mostraSenha').innerText = senha.value;
+
+    document.getElementsByTagName('h2').style.display = "none";
+}
+
+document.replaceChild(exibir, cadastrar, dados);
 
 
+function nomeValido(){
+    if(nome.value == "")
+    {
+        nome.style.backgroundColor="#FFFFFF";
+        nome.style.transitionDuration="0.3s";
+        span[0].style.display="none";
+    }
+    else if(nome.value.length < 10)
+    {
+        nome.style.backgroundColor="#FF9C9D";
+        nome.style.transitionDuration="0.3s";
+        span[0].style.display="block";
+        span[0].style.transitionDuration="0.3s";
+    }
+    else
+    {
+        nome.style.backgroundColor="#92FFC0";
+        nome.style.transitionDuration="0.3s";
+        span[0].style.display="none";
+        span[0].style.transitionDuration="0.3s";
+    }
+}
 
+function loginValido(){
+    if(login.value == "")
+    {
+        login.style.backgroundColor="#FFFFFF";
+        login.style.transitionDuration="0.3s";
+        span[1].style.display="none";
+    }
+    else if(login.value.length > 10)
+    {
+        login.style.backgroundColor="#FF9C9D";
+        login.style.transitionDuration="0.3s";
+        span[1].style.display="block";
+    }
+    else
+    {
+        login.style.backgroundColor="#92FFC0";
+        login.style.transitionDuration="0.3s";
+        span[1].style.display="none";
+    }
+}
+
+function mailValido(){
+    if(email.value == "")
+    {
+        email.style.backgroundColor="#FFFFFF";
+        email.style.transitionDuration="0.3s";
+        span[2].style.display="none";
+    }
+    else if(!emailRegex.test(email.value))
+    {
+        email.style.backgroundColor="#FF9C9D";
+        email.style.transitionDuration="0.3s";
+        span[2].style.display="block";
+    }
+    else
+    {
+        email.style.backgroundColor="#92FFC0";
+        email.style.transitionDuration="0.3s";
+        span[2].style.display="none";
+    }
+}
+
+function senhaValida(){
+    if(senha.value == "" && cfsenha.value == "" )
+    {
+        senha.style.backgroundColor="#FFFFFF";
+        senha.style.transitionDuration="0.3s";
+        cfsenha.style.backgroundColor="#FFFFFF";
+        cfsenha.style.transitionDuration="0.3s";
+        span[3].style.display="none";
+        span[4].style.display="none";
+    }
+    else if(senha.value == cfsenha.value)
+    {
+        senha.style.backgroundColor="#92FFC0";
+        senha.style.transitionDuration="0.3s";
+        cfsenha.style.backgroundColor="#92FFC0";
+        cfsenha.style.transitionDuration="0.3s";
+        span[3].style.display="none";
+        span[4].style.display="none";
+    }
+    else
+    {
+        senha.style.backgroundColor="#FF9C9D";
+        senha.style.transitionDuration="0.3s";
+        cfsenha.style.backgroundColor="#FF9C9D";
+        cfsenha.style.transitionDuration="0.3s";
+        span[3].style.display="block";
+        span[4].style.display="block";
+    }
+}
+
+function botaoDados(){
+    document.getElementById('cadastro_dados').style.display="none";
+    document.getElementById('conteudo').style.display="block";
+}
